b6ab528a915abb8c2b5fcca5129b49c22d75b4fb
Taimi <minna@Minnas-MacBook-Pro.local>
2021-03-29 14:51:52 +0300

    counter app second push

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
+import "./App.css";
 
-const App = () => {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    );
-};
+class App extends Component {
+    state = {
+        counter : 0,
+    };
+
+   addHandler = () => {
+    this.setState({
+    counter: this.state.counter + 1,
+    });
+   };
+
+   removeHandler = () => {
+    if (this.state.counter !== 0) {
+    this.setState({counter: this.state.counter - 1});
+    }
+   };
+
+   resetHandler = () => {
+   this.setState({
+    counter: 0,
+    });
+    };
+
+
+    render() {
+        let circleClass = `${
+            this.state.counter === 0
+            ? ""
+            : this.state.counter % 2 === 0
+            ? "even"
+            : "odd"
+            } circle`;
+
+        return (
+            <div>
+                <h1 className={circleClass}>{this.state.counter}</h1>
+                <button onClick={this.addHandler}>Add one</button>
+                <button onClick={this.removeHandler}>Remove one</button>
+                <button onClick={this.resetHandler}>Reset</button>
+            </div>
+        )
+    }
+}
 
 export default App;
